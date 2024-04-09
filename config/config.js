257cc2dc8dbfde6b079e5bb75a4b62e06dd46a8e
Ankit Tomar <ankitjdec@gmail.com>
2024-04-09 19:54:09 +0530

    added uri link

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.port || 5000,
-  mongoURI: "mongodb://localhost:27017/Social",
-  //mongoURI: "mongodb+srv://ankitjdec:Ankit1234@cluster0.qj7swvw.mongodb.net/",
+  //mongoURI: "mongodb://localhost:27017/Social",
+  mongoURI: "mongodb+srv://ankitjdec:7Secj6SHRuYt0OI5@user.imuswl1.mongodb.net/",
   secretkey: "abcdefgh",
 };
